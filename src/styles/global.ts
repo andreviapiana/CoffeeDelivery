bc4f21ef0cc1e384c76274b6a9ce4ad7070858dd
André Viapiana <andreviapiana@gmail.com>
2023-01-17 10:24:40 -0300

    feat: Start - Estrutura e Temas Globais + ESLint

---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :root {
+    font-size: 62.5%;
+  }
+
+  body {
+    background: ${(props) => props.theme.background};
+    color: ${(props) => props.theme['base-text']};
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1.6rem;
+  }
+`
