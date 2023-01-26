54a3c13fb5dca1efa971704ebec143078b07a5f7
André Viapiana <andreviapiana@gmail.com>
2023-01-26 19:43:09 -0300

    feat: Separação dos Seletores/Radio de Filtragem

---
+++ data/selectors.ts
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+export const categorySelectorTypes = {
+  todos: {
+    label: 'Todos',
+  },
+  tradicional: {
+    label: 'Tradicional',
+  },
+  especial: {
+    label: 'Especial',
+  },
+  leite: {
+    label: 'Com leite',
+  },
+  alcoolico: {
+    label: 'Alcoólico',
+  },
+  gelado: {
+    label: 'Gelado',
+  },
+}
