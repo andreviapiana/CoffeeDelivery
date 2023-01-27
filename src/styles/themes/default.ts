2b4cde23f32f72141e6f1dba43a1328cb68a3c3f
André Viapiana <andreviapiana@gmail.com>
2023-01-27 19:41:43 -0300

    style: Adição da cor dos erros

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -15,6 +15,7 @@ export const defaultTheme = {
   'base-button': '#E6E5E5',
   'base-input': '#EDEDED',
   'base-card': '#F3F2F2',
+  'base-error': '#F84747',
   background: '#FAFAFA',
   white: '#FFFFFF',
 }
